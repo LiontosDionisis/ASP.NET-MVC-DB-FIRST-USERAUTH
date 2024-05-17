35e269bb691a576da3d3bc2225b50a2d8a5143fe
LiontoDionisis <DionisisLiontos@outlook.com>
2024-05-17 22:41:45 +0300

    UserRepository

--- Repositories/UserRepository.cs
+++ Repositories/UserRepository.cs
@@ -1,30 +1,68 @@
-﻿using TeachersMVC.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using TeachersMVC.Data;
 using TeachersMVC.DTO;
+using TeachersMVC.Security;
 
 namespace TeachersMVC.Repositories
 {
     public class UserRepository : BaseRepository<User>, IUserRepository
     {
-        public UserRepository(TeachersMvcdbContext context) : base(context) { }
-
-        public Task<User?> GetByUsernameAsync(string username)
+        private readonly TeachersMvcdbContext context;
+        public UserRepository(TeachersMvcdbContext context) : base(context)
         {
-            throw new NotImplementedException();
+
         }
 
-        public Task<User?> GetUserAsync(string username, string password)
+        public async Task<User?> GetByUsernameAsync(string username)
         {
-            throw new NotImplementedException();
+            return await context.Users.Where(x => x.Username == username).FirstOrDefaultAsync();
         }
 
-        public Task<bool> SignUpUserAsync(UserSignupDTO request)
+        public async Task<User?> GetUserAsync(string username, string password)
         {
-            throw new NotImplementedException();
+            var user = await context.Users.FirstOrDefaultAsync(x => x.Username == username || x.Email == username);
+            if (user == null)
+            {
+                return null;
+            }
+            if (!EncryptionUtil.IsValidPassword(password, user.Password!))
+            {
+                return null;
+            }
+            return user;
         }
 
-        public Task<User> UpdateUserAsync(int userId, UserPatchDTO request)
+        public async Task<bool> SignUpUserAsync(UserSignupDTO request)
         {
-            throw new NotImplementedException();
+            var exisingUser = await context.Users.FirstOrDefaultAsync(x => x.Username == request.Username);
+            if (exisingUser != null) return false;
+
+            var user = new User()
+            {
+                Username = request.Username,
+                Email = request.Email,
+                Firstname = request.Firstname,
+                Lastname = request.Lastname,
+                Password = EncryptionUtil.Encrypt(request.Password!),
+                PhoneNumber = request.PhoneNumber,
+                UserRole = request.UserRole
+            };
+
+            await context.Users.AddAsync(user);
+            return true;
+           
+        }
+
+        public async Task<User> UpdateUserAsync(int userId, UserPatchDTO request)
+        {
+            var user = await context.Users.Where(x => x.Id == userId).FirstAsync();
+
+            user.Email = request.Email;
+            user.Password = EncryptionUtil.Encrypt(request.Password!);
+            user.PhoneNumber = request.PhoneNumber;
+
+            context.Users.Update(user);
+            return user;
         }
     }
 }
