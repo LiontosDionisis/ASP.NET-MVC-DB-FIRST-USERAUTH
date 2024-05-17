b1b30165fd0fc54128b0d724e021161a1357b46f
LiontoDionisis <DionisisLiontos@outlook.com>
2024-05-17 20:35:50 +0300

    Test

--- obj/Release/net8.0/TeachersMVC.AssemblyInfo.cs
+++ obj/Release/net8.0/TeachersMVC.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("TeachersMVC")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+604cdff90db8bf0b75d1b16ea6682edcf8b4c907")]
 [assembly: System.Reflection.AssemblyProductAttribute("TeachersMVC")]
